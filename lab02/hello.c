6cabc8b5a59117109ca3a1c3c9fa2a090a798b92
baegyu3 <baegyu3@ajou.ac.kr>
2021-08-03 16:30:42 +0900

    lab02

---
+++ lab02/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+	int in_data = 0;
+	setvbuf(stdout, NULL, _IOFBF, 100);
+	while (in_data != 9999)
+	{
+	fscanf(stdin, "%d", &in_data);
+	fprintf(stdout, "Hello stdout : %d Baegyu Jung\n", in_data);
+	fprintf(stderr, "Hello stderr : %d Baegyu Jung\n", in_data);
+	}
+}
