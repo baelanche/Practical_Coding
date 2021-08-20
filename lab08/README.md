4d1cfc7d75ad27fc6a2d1d04ba4342180d2e958a
baegyu3 <baegyu3@ajou.ac.kr>
2021-08-20 11:25:15 +0900

    update README

---
+++ lab08/README.md
@@ -0,0 +1,26 @@
+#lab08
+* macro
+* debug
+
+## macro
+* 매크로는 컴파일되면 사용한 코드 부분이 그대로 치환되므로 디버깅이 힘듦
+* 사용할거면 함수를 완성하고 바꾸는 것이 좋음
+* 속도 : 함수 < 매크로
+
+## debug
+```
+cc -g [file] : 출력파일이 디버깅 가능해진다
+gdb [file] : [file] 디버깅
+run : 프로그램 동작
+list : 코드보기
+list [index] : [index] 를 중심으로 코드 보기
+break [index] : [index] 에 중단점 생성
+delete [index] : [index] 의 중단점 삭제
+break : 중단점 확인
+next : 다음 breakpoint로 이동
+ni / next / step : 다음 단계 수행(단위의 차이가 있음)
+coni : continue
+quit : 종료
+
+file [file] 로 파일 정보를 볼때 debug_info 라는 문구가 추가되어있음을 알 수 있다
+```
--- lab08/fx_s32_31.c
+++ lab08/fx_s32_31.c
@@ -1,89 +1,109 @@
-#include <math.h>
 #include <stdio.h>
 
-#define FX_S15_16  1516   	// 교수
-#define FX_S16_15  1615   	// 
-#define FX_S32_31  3231   	// 
-#define FX_S47_16  4716   	// 
-#define FX_S23_08  2308   	// 
+#define FX_S15_16 1516
+#define FX_S16_15 1615
+#define FX_S32_31 3231
+#define FX_S47_16 4716
+#define FX_S23_08 2308
+#define FX_SYSTEM FX_S32_31
 
-#define FX_SYSTEM  FX_S32_31	// 자기것
-	// MAX = +32767.9975    MIN = -32768.00
-
-#if FX_SYSTEM==FX_S32_31		// 자기
-#define FX_QNUM	    	31	    	// 쩜 다음에 몇비트 있음 
-#define FX_QNUM_HALF1	16	    	// 쩜 다음에 몇비트 있음 
-#define FX_QNUM_HALF2	16	    	// 쩜 다음에 몇비트 있음 
-#define FX_QFLOAT_VAL	4294967296.0F
-#define FX_QDOUBLE_VAL	4294967296.0
-#define FX_QINT_VAL	(1<<FX_QNUM)
+#if FX_SYSTEM==FX_S32_31
+#define FX_QNUM 31 // the number of Fractional parts
+#define FX_QNUM_HALF1 15
+#define FX_QNUM_HALF2 16
+#define FX_QFLOAT_VAL 2147483648.0F
+#define FX_QDOUBLE_VAL 2147483648.0
+#define FX_QINT_VAL (1<<FX_QNUM)
 
 typedef long long fx_s32_31;
 typedef fx_s32_31 fixed;
 
-#define fromFloat(a)    ((fixed) ((a)*(FX_QFLOAT_VAL)))
-#define toFloat(a)	((float) ((a)/FX_QFLOAT_VAL))
-#define toDouble(a)	(((a)/FX_QFLOAT_VAL))
+#define fromFloat(a)	((fixed) ((a)*(FX_QFLOAT_VAL)))
+#define toFloat(a)	((float) ((a)/(FX_QFLOAT_VAL)))
+#define toDouble(a)	((a)/(FX_QFLOAT_VAL))
 #define fx_mul2(a,b)	(fromFloat(((toFloat(a))*(toFloat(b)))))
-#define fx_mul3(a,b)	(((a)*(b))>>FX_QNUM)
+#define fx_mul3(a,b)	(((long long)(a)*(b))>>FX_QNUM)
 #define fx_mul4(a,b)	((((long long)(a))*((long long)(b)))>>FX_QNUM)
 #define fx_mul5(a,b)	(((a)>>FX_QNUM_HALF1)*((b)>>FX_QNUM_HALF2))
 
-long long test_mul2() {
-	long long sum = 0;
-	for(int i=0; i<1000000; i++)
-		sum += fx_mul2(i, i+2);
-	return sum;
+long long test_mul2()
+{
+    long long sum = 0;
+    for (int i=0; i < 1000000; i++)
+    {
+        sum += fx_mul2(i, i+2);
+    }
+    return sum;
 }
 
-long long test_mul3() {
-	long long sum = 0;
-	for(int i=0; i<1000000; i++)
-		sum += fx_mul3(i, i+2);
-	return sum;
+long long test_mul3()
+{
+    long long sum = 0;
+    for (int i=0; i < 1000000; i++)
+    {
+        sum += fx_mul3(i, i+2);
+    }
+    return sum;
 }
 
-long long test_mul4() {
-	long long sum = 0;
-	for(int i=0; i<1000000; i++)
-		sum += fx_mul4(i, i+2);
-	return sum;
+long long test_mul4()
+{
+    long long sum = 0;
+    for (int i=0; i < 1000000; i++)
+    {
+        sum += fx_mul4(i, i+2);
+    }
+    return sum;
 }
 
-long long test_mul5() {
-	long long sum = 0;
-	for(int i=0; i<1000000; i++)
-		sum += fx_mul5(i, i+2);
-	return sum;
+long long test_mul5()
+{
+    long long sum = 0;
+    for (int i=0; i < 1000000; i++)
+    {
+        sum += fx_mul5(i, i+2);
+    }
+    return sum;
 }
 
 int main()
 {
-	fixed a, b, c;
+    	fixed a, b, c;
 	float fa,fb,fc; 
 	scanf("%f %f",&fa, &fb); 
+
 	a = fromFloat(fa); 
 	b = fromFloat(fb); 
 	printf("%f %lld %f\n", fa, a, toFloat(a)); 
 	printf("%f %lld %f\n", fb, b, toFloat(b)); 
+
 	c = fx_mul5(a,b); 
 	fc = toFloat(c); 
 	printf("mul5 : %lld %f\n", c, fc); 
+
 	c = fx_mul4(a,b); 
 	fc = toFloat(c); 
 	printf("mul4 : %lld %f\n", c, fc); 
+
 	c = fx_mul3(a,b); 
 	fc = toFloat(c); 
 	printf("mul3 : %lld %f\n", c, fc); 
+
 	c = fx_mul2(a,b); 
 	fc = toFloat(c); 
 	printf("mul2 : %lld %f\n", c, fc); 
 	printf("%f\n", fa*fb); 
-	printf("%e\n", fa*fb);
-	printf("tm2 : %lld\n", test_mul2()); 
-	printf("tm3 : %lld\n", test_mul3()); 
-	printf("tm4 : %lld\n", test_mul4()); 
-	printf("tm5 : %lld\n", test_mul5()); 
-}
+	printf("%e\n", fa*fb); 
 
+  	a = test_mul2();
+    	printf("mul2 : %lld\n", a);
+    	a = test_mul3();
+    	printf("mul3 : %lld\n", a);
+    	a = test_mul4();
+    	printf("mul4 : %lld\n", a);
+    	a = test_mul5();
+    	printf("mul5 : %lld\n", a);
+}
 #endif
+
+
